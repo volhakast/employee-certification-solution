b89f3b10e80e8e41ee4ff5b0738d970ca1d10c76
OlyaKastsiukova <volhakastsiukova@gmail.com>
2026-02-08 15:52:19 +0200

    gh-pages deploy prod

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  }
+  },
+  base: process.env.NODE_ENV === "production" ? "/employee-certification-solution/" : "/"
 });
 
